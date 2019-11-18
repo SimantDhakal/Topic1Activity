f85cef232c5cf107738f2f9c25a28c8ac7717f06
Simant Dhakal <Simant.dhakal@gmail.com>
2019-11-18 22:23:04 +0545

    new Auto complete code

--- app/src/main/java/com/simant/topic1activity/ControllerActivity.java
+++ app/src/main/java/com/simant/topic1activity/ControllerActivity.java
@@ -43,6 +43,7 @@ public class ControllerActivity extends AppCompatActivity {
         autoCompleteTextView.setAdapter(stringArrayAdapter);
         autoCompleteTextView.setThreshold(1);
 
+        // change
 
     }
 }
