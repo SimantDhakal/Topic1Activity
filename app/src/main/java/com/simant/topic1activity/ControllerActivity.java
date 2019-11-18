16b2515d9c2cff8f8d3baccdaa31aaf97a21c28d
Simant Dhakal <Simant.dhakal@gmail.com>
2019-11-18 22:27:55 +0545

    new
    res

--- app/src/main/java/com/simant/topic1activity/ControllerActivity.java
+++ app/src/main/java/com/simant/topic1activity/ControllerActivity.java
@@ -43,7 +43,5 @@ public class ControllerActivity extends AppCompatActivity {
         autoCompleteTextView.setAdapter(stringArrayAdapter);
         autoCompleteTextView.setThreshold(1);
 
-        // change
-
     }
 }
